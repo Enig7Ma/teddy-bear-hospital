432d1af071d6060002dd4bcbe1fb36a29bde04e8
Maria <mariasdruz@gmail.com>
2025-04-20 21:51:51 +0300

    Capture and upload images

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+import uuid
+from datetime import datetime, timezone
+from pathlib import Path
+
+from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
@@ -6,3 +10,19 @@ app = FastAPI()
 @app.get("/api/health")
 def get_health():
     return "Ok"
+
+
+UPLOAD_DIR = Path("./images")
+UPLOAD_DIR.mkdir(exist_ok=True)
+
+
+@app.post("/api/upload")
+async def upload(file: UploadFile = File(...)):
+    assert "." in file.filename
+    ext = Path(file.filename).suffix
+    timestamp = datetime.now(timezone.utc).strftime("%Y%m%d_%H%M%S")
+    suffix = uuid.uuid4()
+    with (
+        (UPLOAD_DIR / f"{timestamp}_{suffix}").with_suffix(ext).open("wb") as targetFile
+    ):
+        targetFile.write(await file.read())
